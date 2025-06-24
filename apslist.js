0574787733c8ec9b23f1e404a0e86005a1817f8e
Kozak809 <kozak800009@gmail.com>
2025-06-24 13:51:17 +0300

    Initial commit: V1.0.0

---
+++ apslist.js
@@ -0,0 +1,11 @@
+const apps = [
+    { name: 'Browser', folder: 'browser'},
+    { name: 'Privacy Policy', folder: 'policty'},
+    { name: 'Settings', folder: 'settings'},
+    { name: 'Calculator', folder: 'calc', defaultWidth: 400, defaultHeight: 620 },
+    { name: 'Remove Duplicates', folder: 'rmdubl'},
+    { name: 'Background Changer', folder: 'background' },
+    { name: 'Remove BG', folder: 'rmbg' },
+    { name: 'Image Converter', folder: 'convert'},
+    { name: 'Image Compressor', folder: 'compress', defaultWidth: 500, defaultHeight: 600 },
+];
